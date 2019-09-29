5a68576df5ee719faabb35973b6f30bd8f8c778f
barryiwhite <barryiwhite@gmail.com>
2019-09-30 00:08:21 +0100

    Don't reorder columns based on group keys and calculate group size from approximate data size

--- tablasco-spark/src/main/java/com/gs/tablasco/spark/DistributedTable.java
+++ tablasco-spark/src/main/java/com/gs/tablasco/spark/DistributedTable.java
@@ -4,23 +4,36 @@ import org.apache.spark.api.java.JavaRDD;
 
 import java.util.List;
 
+/**
+ * A distributed table that can be compared by SparkVerifier
+ */
 @SuppressWarnings("WeakerAccess")
 public class DistributedTable
 {
     private final List<String> headers;
     private final JavaRDD<List<Object>> rows;
 
+    /**
+     * @param headers table headers (column names)
+     * @param rows An RDD of table rows with a row represented as a list of objects
+     */
     public DistributedTable(List<String> headers, JavaRDD<List<Object>> rows)
     {
         this.headers = headers;
         this.rows = rows;
     }
 
+    /**
+     * @return the table headers (column names)
+     */
     public List<String> getHeaders()
     {
         return headers;
     }
 
+    /**
+     * @return An RDD of table rows with a row represented as a list of objects
+     */
     public JavaRDD<List<Object>> getRows()
     {
         return rows;
