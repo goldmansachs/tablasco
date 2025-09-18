a787f6573b87eca6064bc08a27dc8f044863427b
barry <barryiwhite@gmail.com>
2025-09-18 23:25:24 +0200

    Remove unused code

--- tablasco-core/src/main/java/com/gs/tablasco/core/Tablasco.java
+++ tablasco-core/src/main/java/com/gs/tablasco/core/Tablasco.java
@@ -1,27 +1,17 @@
 package com.gs.tablasco.core;
 
-import static com.gs.tablasco.rebase.RebaseFileWriter.printTable;
-
 import com.gs.tablasco.NamedTable;
 import com.gs.tablasco.VerifiableTable;
-import com.gs.tablasco.results.ExpectedResults;
-import com.gs.tablasco.results.parser.ExpectedResultsParser;
 import com.gs.tablasco.verify.HtmlFormatter;
 import com.gs.tablasco.verify.MultiTableVerifier;
 import com.gs.tablasco.verify.ResultTable;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Tablasco {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Tablasco.class);
 
     private final VerifierConfig verifierConfig;
     private final HtmlConfig htmlConfig;
@@ -31,27 +21,6 @@ public class Tablasco {
         this.verifierConfig = verifierConfig;
         this.htmlConfig = htmlConfig;
         this.testName = testName;
-    }
-
-    public Map<String, VerifiableTable> loadBaseline(InputStream baselineInput) {
-        ExpectedResultsParser expectedResultsParser = new ExpectedResultsParser(expectedFile -> baselineInput, null);
-        ExpectedResults expectedResults = expectedResultsParser.parse();
-        return expectedResults.getTables(this.testName);
-    }
-
-    public void saveBaseline(List<NamedTable> tables, OutputStream outputStream) {
-        try (PrintWriter printWriter =
-                new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)))) {
-            for (NamedTable table : tables) {
-                LOGGER.info("Writing results for '{}'", table.getName());
-                printTable(
-                        printWriter,
-                        this.testName,
-                        table.getName(),
-                        table.getTable(),
-                        this.verifierConfig.getColumnComparators());
-            }
-        }
     }
 
     public Map<String, ResultTable> verifyTables(List<NamedTable> expectedTables, List<NamedTable> actualTables) {
